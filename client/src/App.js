f63bd61230f44e259b744aa5c82cf7ee28b4fdf6
sorrelbri <sorrel.bri.june@gmail.com>
2020-07-21 15:37:54 -0400

    stub BalancePage

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { MuiThemeProvider } from "@material-ui/core";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import { theme } from "./themes/theme";
 import LandingPage from "./pages/Landing";
+import BalancePage from "./pages/Balance";
 
 import "./App.css";
 
@@ -11,7 +12,13 @@ function App() {
   return (
     <MuiThemeProvider theme={theme}>
       <BrowserRouter>
-        <Route path="/" component={LandingPage} />
+        <Switch>
+          {/* authz */}
+          <Route path="/balance">
+            <BalancePage />
+          </Route>
+          <Route path="/" component={LandingPage} />
+        </Switch>
       </BrowserRouter>
     </MuiThemeProvider>
   );
---
+++ client/src/pages/Balance.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BalancePage = ({ ...props }) => {
+  return (
+    <>
+      <h3>Your Balance</h3>
+    </>
+  );
+};
+
+export default BalancePage;
