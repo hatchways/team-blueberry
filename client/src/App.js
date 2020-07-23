b2b6cefab5a61119c6dff78181207abdf0e7733e
sorrelbri <sorrel.bri.june@gmail.com>
2020-07-23 12:43:44 -0400

    add initState function to lazily set initial app state

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useReducer } from "react";
 import { MuiThemeProvider } from "@material-ui/core";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import reducer from "./reducers";
+import initState from "./initState"
 
 import { userContext } from "./userContext";
 import { theme } from "./themes/theme";
@@ -11,7 +12,7 @@ import SignUp from "./pages/SignUp";
 import "./App.css";
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, {});
+  const [state, dispatch] = useReducer(reducer, {}, initState);
 
   return (
     <userContext.Provider value={state.user}>
