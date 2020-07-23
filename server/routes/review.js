a8c68ee2dc764c14d55f56fbb03b1772267f53a0
Tyler <tylerpoore2000@gmail.com>
2020-07-22 18:25:47 -0700

    Added try catch blocks to review / request route code

--- server/routes/review.js
+++ server/routes/review.js
@@ -8,29 +8,37 @@ const User = require("../models/user");
 const Review = require("../models/review");
 
 router.post("/createReview", async (req, res) => {
-  const email = req.body.email;
-  const user = await User.findOne({ email: email });
+  try {
+    const email = req.body.email;
+    const user = await User.findOne({ email: email });
 
-  const data = {
-    language: req.body.language,
-    title: req.body.title,
-    codeSnippet: req.body.codeSnippet,
-    messageText: req.body.messageText,
-  };
+    const data = {
+      language: req.body.language,
+      title: req.body.title,
+      codeSnippet: req.body.codeSnippet,
+      messageText: req.body.messageText,
+    };
 
-  await reviewModel.createReview(user, data, () => {
-    res.status(201).json({ message: "Complete" });
-  });
+    await reviewModel.createReview(user, data, () => {
+      res.status(201).json({ message: "Complete" });
+    });
+  } catch {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
 // gets all relevant reviews
 router.post("/getReviews", async (req, res) => {
-  const email = req.body.email;
-  const user = await User.findOne({ email: email });
+  try {
+    const email = req.body.email;
+    const user = await User.findOne({ email: email });
 
-  const reviews = await Review.find({ userId: user._id });
+    const reviews = await Review.find({ userId: user._id });
 
-  res.status(201).json({ result: reviews });
+    res.status(201).json({ result: reviews });
+  } catch {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
 module.exports = router;
