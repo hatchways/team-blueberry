c30c37eaea088b30bdf5a139949754e2fb95b373
Affeeq Ismail <affeeqabedeenismail@gmail.com>
2020-07-23 12:02:20 -0400

    edit ping.spec.js

--- server/test/ping.spec.js
+++ server/test/ping.spec.js
@@ -4,18 +4,3 @@ const app = require("../app.js");
 
 chai.should();
 chai.use(chaiHttp);
-
-describe("/POST ping", () => {
-  it("it should return 400", (done) => {
-    chai
-      .request(app)
-      .send({ teamName: "Shums" })
-      .end((err, res) => {
-        res.should.have.status(400);
-        res.body.should.have
-          .property("response")
-          .eql("Shums is not part of the team. Modify your .env");
-        done();
-      });
-  });
-});
