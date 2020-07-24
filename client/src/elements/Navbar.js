29cf1762aed729db36638f4e0dc979c1d98711ba
Affeeq Ismail <affeeqabedeenismail@gmail.com>
2020-07-24 13:15:39 -0400

    added responsiveness to navbar

--- client/src/elements/Navbar.js
+++ client/src/elements/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Button from "@material-ui/core/Button";
@@ -9,12 +9,17 @@ import Avatar from "@material-ui/core/Avatar";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
 import Badge from "@material-ui/core/Badge";
+import MenuIcon from "@material-ui/icons/Menu";
 import CodeIcon from "@material-ui/icons/Code";
 import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 import { withRouter } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
+    flexGrow: 1,
+  },
+  title: {
     flex: 1,
   },
   button: {
@@ -49,81 +54,95 @@ const useStyles = makeStyles((theme) => ({
 const Navbar = () => {
   const classes = useStyles();
 
+  // navbar logic
   const [anchorNotificaton, setAnchorNotificaton] = React.useState(null);
-  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [anchorMenu, setAnchorMenu] = React.useState(null);
 
   const handleNotificaton = (event) => {
     setAnchorNotificaton(event.currentTarget);
   };
 
-  // menu
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
+  const handleClickMenu = (event) => {
+    setAnchorMenu(event.currentTarget);
   };
 
-  const handleClose = () => {
-    setAnchorEl(null);
+  const handleCloseMenu = () => {
+    setAnchorMenu(null);
   };
+
+  // add responsiveness
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
   return (
-    <AppBar position="fixed" color="secondary">
-      <Toolbar>
-        <div className={classes.root}>
-          <CodeIcon />
-        </div>
-        <Button className={classes.button}>Reviews</Button>
-        <Button className={classes.button}>Balance</Button>
-        <Badge color="primary" variant="dot" overlap="circle">
-          <IconButton
-            aria-label="account of current user"
-            aria-controls="menu-appbar"
-            aria-haspopup="true"
-            onClick={handleNotificaton}
-            color="inherit"
-            className={classes.notification}
-          >
-            <NotificationsNoneIcon />
-          </IconButton>
-        </Badge>
-        <Button
-          className={classes.button}
-          color="primary"
-          variant="outlined"
-          className={classes.upload}
-        >
-          Upload Code
-        </Button>
-        <div className={classes.avatar}>
-          {/* input user's image */}
-          <Avatar src="">UI {/* here is the user initial */}</Avatar>
-        </div>
-        <Button
-          className={classes.buttonMenu}
-          aria-controls="customized-menu"
-          aria-haspopup="true"
-          variant="text"
-          color="primary"
-          onClick={handleClick}
-        >
-          Profile
-          <ArrowDropDownIcon />
-          {/* // menu goes here */}
-        </Button>
-        <Menu
-          id="nav-menu"
-          anchorEl={anchorEl}
-          getContentAnchorEl={null}
-          open={Boolean(anchorEl)}
-          onClose={handleClose}
-          anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
-          transformOrigin={{ vertical: "top", horizontal: "center" }}
-        >
-          <MenuItem onClick={handleClose}>Profile</MenuItem>
-          <MenuItem onClick={handleClose}>My account</MenuItem>
-          <MenuItem onClick={handleClose}>Logout</MenuItem>
-        </Menu>
-      </Toolbar>
-    </AppBar>
+    <div className={classes.root}>
+      <AppBar position="fixed" color="secondary">
+        <Toolbar>
+          <div className={classes.title}>
+            <CodeIcon fontSize="large" />
+          </div>
+          {isMobile ? (
+            // need to decide styling on mobile
+            <IconButton edge="start" color="inherit">
+              <MenuIcon fontSize="large" />
+            </IconButton>
+          ) : (
+            <>
+              <Button className={classes.button}>Reviews</Button>
+              <Button className={classes.button}>Balance</Button>
+              <Badge color="primary" variant="dot" overlap="circle">
+                <IconButton
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleNotificaton}
+                  color="inherit"
+                  className={classes.notification}
+                >
+                  <NotificationsNoneIcon />
+                </IconButton>
+              </Badge>
+              <Button
+                className={classes.button}
+                color="primary"
+                variant="outlined"
+                className={classes.upload}
+              >
+                Upload Code
+              </Button>
+              <div className={classes.avatar}>
+                {/* input user's image */}
+                <Avatar src="">UI {/* here is the user initial */}</Avatar>
+              </div>
+              <Button
+                className={classes.buttonMenu}
+                aria-controls="customized-menu"
+                aria-haspopup="true"
+                variant="text"
+                color="primary"
+                onClick={handleClickMenu}
+              >
+                Profile
+                <ArrowDropDownIcon />
+                {/* // menu goes here */}
+              </Button>
+              <Menu
+                id="nav-menu"
+                anchorEl={anchorMenu}
+                getContentAnchorEl={null}
+                open={Boolean(anchorMenu)}
+                onClose={handleCloseMenu}
+                anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+                transformOrigin={{ vertical: "top", horizontal: "center" }}
+              >
+                <MenuItem onClick={handleCloseMenu}>Profile</MenuItem>
+                <MenuItem onClick={handleCloseMenu}>My account</MenuItem>
+                <MenuItem onClick={handleCloseMenu}>Logout</MenuItem>
+              </Menu>
+            </>
+          )}
+        </Toolbar>
+      </AppBar>
+    </div>
   );
 };
 
