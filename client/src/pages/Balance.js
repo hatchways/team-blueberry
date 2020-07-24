91866de2a8525615866f8e4597479bc2d4105f1e
sorrelbri <sorrel.bri.june@gmail.com>
2020-07-24 13:36:36 -0400

    add userContext to Balance Page, add protected route to Balance Page

--- client/src/pages/Balance.js
+++ client/src/pages/Balance.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import userContext from "../userContext";
+import Background from "../elements/Background";
+import StyledPaper from "../elements/StyledPaper";
+import PageHeader from "../elements/PageHeader";
+import { Container, CssBaseline } from "@material-ui/core";
 
-const BalancePage = ({ ...props }) => {
+const BalancePage = ({ state, dispatch }) => {
+  const user = useContext(userContext);
+  const [topUp, setTopUp] = useState(2);
+
   return (
-    <>
-      <h3>Your Balance</h3>
-    </>
+    <Background solid>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <StyledPaper mt={20}>
+          {/* section */}
+          <PageHeader>Your Balance:</PageHeader>
+          {/* `${user.balance} credits` */}
+          {/* hr */}
+          {/* section */}
+          <h4>Top Up:</h4>
+          {/* form incr, decr, value */}
+          {/* form submit checkout */}
+        </StyledPaper>
+      </Container>
+    </Background>
   );
 };
 
