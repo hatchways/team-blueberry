f352372b116686b41bc19532eaec22caf26a4765
Tyler Poore <43526510+LiveBacteria@users.noreply.github.com>
2020-07-24 19:04:39 -0700

    Completed languages.js, requsetHandler.js, onBoarding,js files
    Completed languages.js, requsetHandler.js, onBoarding,js files. Now need to implement

--- client/src/services/languages.js
+++ client/src/services/languages.js
@@ -4,13 +4,28 @@ const request = require("../utils/requestHandlers");
 module.exports = {
   async getLanguages() {
     //Make get call to API for languages
-    const config = {};
+    const config = {
+      method: "get",
+      url: "/api/languages",
+    };
 
-    request.
-    return { languages: ["Javascript"] };
+    const data = await request.getFromApi().data;
+    // return { languages: ["Javascript"] };
   },
   async updateLanguages(languages) {
     //Make put call to api for updating languages on current user
-    return { message: "success" };
+    const config = {
+      method: "put",
+
+      //This may need to be changed to a relevant route, but since it is updating user I believe it should be here
+      url: "/api/user/languages",
+      data: {
+        languages,
+      },
+    };
+
+    const data = await request.putToAPI(config).data;
+    return data;
+    // return { message: "success" };
   },
 };
--- client/src/utils/requestHandlers.js
+++ client/src/utils/requestHandlers.js
@@ -2,6 +2,26 @@ import axios from "axios";
 
 //Handle all requests to API here
 module.exports = {
-  getFromAPI(config) {},
-  putToAPI(config) {},
+  async getFromAPI(config) {
+    try {
+      //Logic to update reducer
+      const response = await axios.get(config);
+    } catch (error) {
+      //Logic to update reducer
+      console.error(error);
+    }
+    return response;
+    //Logic to update reducer
+  },
+  async putToAPI(config) {
+    try {
+      //Logic to update reducer
+      const response = await axios.put(config);
+    } catch (error) {
+      //Logic to update reducer
+      console.error(error);
+    }
+    return response;
+    //Logic to update reducer
+  },
 };
