bac06c5fb730ecea1af0e6253dad7a608dbd3a06
svitaly <vitalii.sidorok@gmail.com>
2020-08-27 20:57:15 -0400

    Removed comments

--- server/routes/payment.js
+++ server/routes/payment.js
@@ -1,15 +1,8 @@
 const { Router } = require("express");
 const router = Router();
-// const {
-//   getKey,
-//   createPayment,
-//   updatePayment,
-// } = require("../controllers/payment");
+
 const { stripePayment } = require("../controllers/stripeServer");
 
 router.post("/", stripePayment);
-// router.get("/key", getKey);
-// router.post("/", createPayment);
-// router.put("/:paymentIntentId", updatePayment);
 
 module.exports = router;
