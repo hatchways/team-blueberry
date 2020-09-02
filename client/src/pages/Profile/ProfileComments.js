bd0668e84150e2dd9b9d2fb785a038664eb2e477
Affeeq Ismail <affeeqabedeenismail@gmail.com>
2020-09-02 10:41:33 -0400

    added pagination

--- client/src/pages/Profile/ProfileComments.js
+++ client/src/pages/Profile/ProfileComments.js
@@ -11,12 +11,18 @@ import CardHeader from "@material-ui/core/CardHeader";
 import CardContent from "@material-ui/core/CardContent";
 import Rating from "@material-ui/icons/FiberManualRecord";
 import RatingOutlined from "@material-ui/icons/FiberManualRecordOutlined";
+import Box from "@material-ui/core/Box";
+import IconButton from "@material-ui/core/IconButton";
+import ArrowBackward from "@material-ui/icons/ArrowBackIosOutlined";
+import ArrowForward from "@material-ui/icons/ArrowForwardIosOutlined";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     width: "100%",
-    // maxWidth: "600px",
     backgroundColor: theme.palette.background.paper,
+  },
+  paper: {
+    width: "100%",
   },
   inline: {
     display: "inline",
@@ -24,86 +30,175 @@ const useStyles = makeStyles((theme) => ({
   ratingColor: {
     fill: theme.palette.primary.main,
   },
+  cardHeader: {
+    minWidth: "260px",
+  },
 }));
 
 const ProfileComments = ({ comments }) => {
   const classes = useStyles();
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(3);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  // Get current posts
+  const commentsPerPage = 3;
+  const indexOfLastPost = currentPage * commentsPerPage;
+  const indexOfFirstPost = indexOfLastPost - commentsPerPage;
+  const currentComments = comments.slice(indexOfFirstPost, indexOfLastPost);
+  const totalPages = Math.ceil(comments.length / commentsPerPage);
+
+  const handleBack = () => {
+    setCurrentPage(currentPage - 1);
+  };
+
+  const handleForward = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
+  const handlePage = (page) => {
+    setCurrentPage(page);
+  };
 
   return (
-    <Paper elevation={1}>
+    <Paper elevation={1} className={classes.paper}>
       <List className={classes.root}>
-        {comments
-          .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-          .map((comment, index) => (
-            <React.Fragment key={index}>
-              <ListItem alignItems="flex-start">
-                <CardHeader
-                  avatar={
-                    comment.avatar ? (
-                      <Avatar src={comment.avatar}></Avatar>
-                    ) : (
-                      <Avatar src={AvatarImage} />
-                    )
-                  }
-                  title={comment.name}
-                  subheader={
-                    comment.position ? comment.position : "Not Specififed"
-                  }
-                />
-                <CardContent>
-                  {comment.embeddedReview.rating < 2 ? (
+        {currentComments.map((comment, index) => (
+          <React.Fragment key={index}>
+            <ListItem alignItems="flex-start">
+              <CardHeader
+                avatar={
+                  comment.avatar ? (
+                    <Avatar src={comment.avatar}></Avatar>
+                  ) : (
+                    <Avatar src={AvatarImage} />
+                  )
+                }
+                title={comment.name}
+                subheader={
+                  comment.position ? comment.position : "Not Specififed"
+                }
+                className={classes.cardHeader}
+              />
+              <CardContent>
+                {comment.embeddedReview.rating < 2 ? (
+                  <React.Fragment>
+                    <Rating className={classes.ratingColor} />
+                    <RatingOutlined className={classes.ratingColor} />
+                    <RatingOutlined className={classes.ratingColor} />
+                    <RatingOutlined className={classes.ratingColor} />
+                    <RatingOutlined className={classes.ratingColor} />
+                  </React.Fragment>
+                ) : comment.embeddedReview.rating < 3 ? (
+                  <React.Fragment>
+                    <Rating className={classes.ratingColor} />
+                    <Rating className={classes.ratingColor} />
+                    <RatingOutlined className={classes.ratingColor} />
+                    <RatingOutlined className={classes.ratingColor} />
+                    <RatingOutlined className={classes.ratingColor} />
+                  </React.Fragment>
+                ) : comment.embeddedReview.rating < 4 ? (
+                  <React.Fragment>
+                    <Rating className={classes.ratingColor} />
+                    <Rating className={classes.ratingColor} />
+                    <Rating className={classes.ratingColor} />
+                    <RatingOutlined className={classes.ratingColor} />
+                    <RatingOutlined className={classes.ratingColor} />
+                  </React.Fragment>
+                ) : comment.embeddedReview.rating < 5 ? (
+                  <React.Fragment>
+                    <Rating className={classes.ratingColor} />
+                    <Rating className={classes.ratingColor} />
+                    <Rating className={classes.ratingColor} />
+                    <Rating className={classes.ratingColor} />
+                    <RatingOutlined className={classes.ratingColor} />
+                  </React.Fragment>
+                ) : (
+                  <React.Fragment>
+                    <Rating className={classes.ratingColor} />
+                    <Rating className={classes.ratingColor} />
+                    <Rating className={classes.ratingColor} />
+                    <Rating className={classes.ratingColor} />
+                    <Rating className={classes.ratingColor} />
+                  </React.Fragment>
+                )}
+
+                <Typography className={classes.comment}>
+                  {comment.comment ? comment.comment : "Not Specified"}
+                </Typography>
+              </CardContent>
+            </ListItem>
+            <Divider />
+          </React.Fragment>
+        ))}
+        {comments.length ? (
+          <Box>
+            <IconButton disabled={currentPage === 1} onClick={handleBack}>
+              <ArrowBackward />
+            </IconButton>
+            {totalPages >= 3 ? (
+              <React.Fragment>
+                {currentPage === 1 ? (
+                  <React.Fragment>
+                    <IconButton disabled={true}>{currentPage}</IconButton>
+                    <IconButton onClick={() => handlePage(currentPage + 1)}>
+                      {currentPage + 1}
+                    </IconButton>
+                    <IconButton onClick={() => handlePage(currentPage + 2)}>
+                      {currentPage + 2}
+                    </IconButton>
+                  </React.Fragment>
+                ) : currentPage === totalPages ? (
+                  <React.Fragment>
+                    <IconButton onClick={() => handlePage(currentPage - 2)}>
+                      {currentPage - 2}
+                    </IconButton>
+                    <IconButton onClick={() => handlePage(currentPage - 1)}>
+                      {currentPage - 1}
+                    </IconButton>
+                    <IconButton disabled={true}>{currentPage}</IconButton>
+                  </React.Fragment>
+                ) : (
+                  <React.Fragment>
+                    <IconButton onClick={() => handlePage(currentPage - 1)}>
+                      {currentPage - 1}
+                    </IconButton>
+                    <IconButton disabled={true}>{currentPage}</IconButton>
+                    <IconButton onClick={() => handlePage(currentPage + 1)}>
+                      {currentPage + 1}
+                    </IconButton>
+                  </React.Fragment>
+                )}
+              </React.Fragment>
+            ) : totalPages === 2 ? (
+              <React.Fragment>
+                <React.Fragment>
+                  {currentPage === 1 ? (
                     <React.Fragment>
-                      <Rating className={classes.ratingColor} />
-                      <RatingOutlined className={classes.ratingColor} />
-                      <RatingOutlined className={classes.ratingColor} />
-                      <RatingOutlined className={classes.ratingColor} />
-                      <RatingOutlined className={classes.ratingColor} />
-                    </React.Fragment>
-                  ) : comment.embeddedReview.rating < 3 ? (
-                    <React.Fragment>
-                      <Rating className={classes.ratingColor} />
-                      <Rating className={classes.ratingColor} />
-                      <RatingOutlined className={classes.ratingColor} />
-                      <RatingOutlined className={classes.ratingColor} />
-                      <RatingOutlined className={classes.ratingColor} />
-                    </React.Fragment>
-                  ) : comment.embeddedReview.rating < 4 ? (
-                    <React.Fragment>
-                      <Rating className={classes.ratingColor} />
-                      <Rating className={classes.ratingColor} />
-                      <Rating className={classes.ratingColor} />
-                      <RatingOutlined className={classes.ratingColor} />
-                      <RatingOutlined className={classes.ratingColor} />
-                    </React.Fragment>
-                  ) : comment.embeddedReview.rating < 5 ? (
-                    <React.Fragment>
-                      <Rating className={classes.ratingColor} />
-                      <Rating className={classes.ratingColor} />
-                      <Rating className={classes.ratingColor} />
-                      <Rating className={classes.ratingColor} />
-                      <RatingOutlined className={classes.ratingColor} />
+                      <IconButton disabled={true}>{currentPage}</IconButton>
+                      <IconButton onClick={() => handlePage(currentPage + 1)}>
+                        {currentPage + 1}
+                      </IconButton>
                     </React.Fragment>
                   ) : (
                     <React.Fragment>
-                      <Rating className={classes.ratingColor} />
-                      <Rating className={classes.ratingColor} />
-                      <Rating className={classes.ratingColor} />
-                      <Rating className={classes.ratingColor} />
-                      <Rating className={classes.ratingColor} />
+                      <IconButton onClick={() => handlePage(currentPage - 1)}>
+                        {currentPage - 1}
+                      </IconButton>
+                      <IconButton disabled={true}>{currentPage}</IconButton>
                     </React.Fragment>
                   )}
-
-                  <Typography className={classes.comment}>
-                    {comment.comment ? comment.comment : "Not Specified"}
-                  </Typography>
-                </CardContent>
-              </ListItem>
-              <Divider />
-            </React.Fragment>
-          ))}
-        {/* TODO Add pagination here */}
+                </React.Fragment>
+              </React.Fragment>
+            ) : (
+              <IconButton disabled={true}>{currentPage}</IconButton>
+            )}
+            <IconButton
+              disabled={currentPage === totalPages}
+              onClick={handleForward}
+            >
+              <ArrowForward />
+            </IconButton>
+          </Box>
+        ) : null}
       </List>
     </Paper>
   );
