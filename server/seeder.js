25935ac14e61f8e4c6d539a004313de2dcc2a80d
svitaly <vitalii.sidorok@gmail.com>
2020-09-05 15:21:09 -0400

    Seeder update - removed payment model

--- server/seeder.js
+++ server/seeder.js
@@ -16,7 +16,7 @@ module.exports = (mongoUri) =>
     console.log(`seeding to ${mongoUri || dbString}`);
     seeder.loadModels([
       "./models/user.js",
-      "./models/payment.js",
+      // "./models/payment.js",
       "./models/review-request.js",
       // ? can this be done programmatically?
     ]);
