18d62638fba02690524080cd51be0ff9ead6b0f9
Tyler Poore <43526510+LiveBacteria@users.noreply.github.com>
2020-09-08 18:15:56 -0700

    changed app.js

---
+++ client/build/precache-manifest.91412a61626c19cae10907a942092741.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6a24fab20c44c8910c7e72baadb3726",
+    "url": "/index.html"
+  },
+  {
+    "revision": "172931fa349779c729ee",
+    "url": "/static/css/2.66a375a2.chunk.css"
+  },
+  {
+    "revision": "41c12cc703ef68ec9ef3",
+    "url": "/static/css/main.f302303f.chunk.css"
+  },
+  {
+    "revision": "172931fa349779c729ee",
+    "url": "/static/js/2.f566bc78.chunk.js"
+  },
+  {
+    "revision": "4974e7d03b3bebfaa7f3542a6fe8327f",
+    "url": "/static/js/2.f566bc78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41c12cc703ef68ec9ef3",
+    "url": "/static/js/main.3aa7fc48.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "8cb04c9257b4a8c80ab792437cfefc9a",
+    "url": "/static/media/avatar.8cb04c92.png"
+  },
+  {
+    "revision": "a979f2bc61a3eb0595716322d37aee83",
+    "url": "/static/media/code.a979f2bc.jpg"
+  },
+  {
+    "revision": "c61a5c47a1d7d4b26982ae4cb8ada1aa",
+    "url": "/static/media/cpp.c61a5c47.svg"
+  },
+  {
+    "revision": "340da926f9151f16a113f5ea78ad5b01",
+    "url": "/static/media/csharp.340da926.svg"
+  },
+  {
+    "revision": "0e445d132f7f7ed175fc9aa6ab1a77d2",
+    "url": "/static/media/golang.0e445d13.svg"
+  },
+  {
+    "revision": "cce27e614f7e248cfc9827467f46c01f",
+    "url": "/static/media/java.cce27e61.svg"
+  },
+  {
+    "revision": "5b54675950638dd95de282256b5cf7c4",
+    "url": "/static/media/javascript.5b546759.svg"
+  },
+  {
+    "revision": "89d81358796656ffbe17038916c84637",
+    "url": "/static/media/php.89d81358.svg"
+  },
+  {
+    "revision": "01766e9cdd6e12e020a2c9ca505fd961",
+    "url": "/static/media/python.01766e9c.svg"
+  },
+  {
+    "revision": "b00bb61365b0dd9f409afe8bfb7bd356",
+    "url": "/static/media/ruby.b00bb613.svg"
+  }
+]);
