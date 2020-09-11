b5be42b74c384ec9b55eae34228eec35d74cf5b5
Tyler Poore <43526510+LiveBacteria@users.noreply.github.com>
2020-09-10 17:17:48 -0700

    changed app.js

--- app.js
+++ app.js
@@ -61,6 +61,14 @@ app.use(cookieParser());
 app.use(bodyParser.raw({ type: "image/jpg", limit: "1mb" }));
 // app.use(express.static(join(__dirname, "public")));
 
+if (process.env.NODE_ENV === "production") {
+  app.use((req, res, next) => {
+    if (req.header("x-forwarded-proto") !== "https")
+      res.redirect(`https://${req.header("host")}${req.url}`);
+    else next();
+  });
+}
+
 app.use("/api", indexRouter);
 app.use("/api/payment", auth, paymentRouter);
 
